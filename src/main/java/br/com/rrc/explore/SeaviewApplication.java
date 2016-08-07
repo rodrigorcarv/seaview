cb403589fd15ada5a331d2c5fbc5140d9ca0abac
rodrigo.carvalho <rodrigo.carvalho@fcamara.com.br>
2016-08-07 09:05:18 -0300

    Criacao da estrutura inicial do projeto

---
+++ src/main/java/br/com/rrc/explore/SeaviewApplication.java
@@ -0,0 +1,12 @@
+package br.com.rrc.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaviewApplication.class, args);
+	}
+}
