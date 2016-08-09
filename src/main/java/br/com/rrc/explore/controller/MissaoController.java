ccbeab58e13114dfb164cc95c50548b34c3f0bad
rodrigo.carvalho <rodrigo.carvalho@fcamara.com.br>
2016-08-09 18:33:42 -0300

    Refatorando controller para extrair implementacao para o service

--- src/main/java/br/com/rrc/explore/controller/MissaoController.java
+++ src/main/java/br/com/rrc/explore/controller/MissaoController.java
@@ -1,7 +1,6 @@
 package br.com.rrc.explore.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rrc.explore.beans.enums.Comando;
-import br.com.rrc.explore.beans.enums.Direcao;
 import br.com.rrc.explore.convert.ComandoConvert;
+import br.com.rrc.explore.service.MissaoService;
 
 @RestController
 @RequestMapping(value="/missao")
@@ -20,70 +19,15 @@ public class MissaoController {
 
 	@Autowired
 	ComandoConvert comandoConvert;
+	
+	@Autowired
+	MissaoService missaoService;
 
 	@RequestMapping(value = "/explorar-fossa", method = RequestMethod.POST, consumes = "text/plain", produces = "text/plain")
 	public @ResponseBody String missao (@RequestBody String comandos){
 
 		List<Comando> listaComandos = comandoConvert.string2Comandos(comandos);
 
-		int posicaoEntradaX = 0;
-		int posicaoEntradaY = 0;
-		int posicaoEntradaZ = 0;
-		Direcao direcao = Direcao.NORTE;
-
-		for (Comando comando : listaComandos) {
-
-			switch (comando) {
-
-			case ESQUERDA:
-
-				direcao = direcao.virarEsqueda();
-				break;
-
-			case DIREITA:
-
-				direcao = direcao.virarDireita();
-				break;
-
-			case MOVER:
-
-				switch (direcao) {
-
-				case NORTE:
-
-					posicaoEntradaY++;
-					break;
-
-				case LESTE:
-
-					posicaoEntradaX++;
-					break;
-
-				case SUL:
-
-					posicaoEntradaY--;
-					break;
-
-				case OESTE:
-
-					posicaoEntradaX--;
-					break;
-				}
-				
-				break;
-
-			case SUBIR:
-
-				posicaoEntradaZ++;
-				break;
-
-			case DESCER:
-
-				posicaoEntradaZ--;
-				break;
-				
-			}
-		}
-		return String.format("%s %s %s %s", posicaoEntradaX, posicaoEntradaY, posicaoEntradaZ, direcao);
+		return missaoService.explorarOceano(listaComandos);
 	}
 }
--- src/test/java/br/com/rrc/explore/convert/TestConvertComando.java
+++ src/test/java/br/com/rrc/explore/convert/TestConvertComando.java
@@ -44,4 +44,6 @@ public class TestConvertComando {
 		
 		Assert.assertEquals(listaComandos, comandos);
 	}
+	
+	
 }
